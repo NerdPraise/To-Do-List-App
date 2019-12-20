59325c5c01438009c6a926f22db956e49a255b66
NerdPraise <praiseajayi2@gmail.com>
2019-12-20 09:50:11 +0100

    initial push

---
+++ listapp/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+
+
+urlpatterns=[
+    path("", views.home, name="home"),
+    path("add_todo", views.add_todo, name="add"),
+    path("delete_todo/<int:todo_id>", views.delete_todo, name="delete"),
+
+]
